9e2cecc87621b63bc5e0269d9ba45924994c3917
Aryanraval2030 <aryanraval1432@gmail.com>
2026-04-17 19:45:47 +0530

    hit

--- back_end/server.js
+++ back_end/server.js
@@ -10,7 +10,15 @@ app.use(express.json());
 
 app.use(cors());
 app.use((req, res, next) => {
-  console.log("API hit ho rahi hai");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
